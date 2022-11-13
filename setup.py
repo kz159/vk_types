985c489eaf8ee0c731c35eac7f6448a867ff8895
Georgy <loh@kz159.ru>
2022-11-13 13:52:33 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="vk_types",
-    version="0.2.3",
+    version="0.2.3.1",
     author="kz159",
     description="vk_types for vk json answers",
     long_description=long_description,
